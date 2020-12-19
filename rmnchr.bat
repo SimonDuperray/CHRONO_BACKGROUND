314196fd0bcc22d0c537e24735bd5d838afa801d
Simon Duperray <64204457+SimonDuperray@users.noreply.github.com>
2020-12-19 10:49:07 +0100

    Add files via upload

--- rmnchr.bat
+++ rmnchr.bat
@@ -1,6 +1,3 @@
 @ECHO OFF
-REM MultiMonitorTool.exe /enable 2
-REM DisplaySwitch.exe /internal
-START chrono.lnk
-"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --kiosk "C:\Windows\System32\chrono_remain\index.html"
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --kiosk "C:\Windows\System32\index_chrono.html"
 EXIT
